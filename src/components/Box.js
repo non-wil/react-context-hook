3d126f4041b03bb0247ba5ad24748721ede7894b
nontaya <nontaya.may@gmail.com>
2019-10-24 02:44:03 +0700

    ✨ 2nd way to consume context with consumer

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import { ThemeContext } from '../contexts/ThemeContext'
 
 export default class Box extends Component {
-  static contextType = ThemeContext
   render() {
-    const { isLightTheme, light, dark } = this.context
-    const theme = isLightTheme ? light : dark
     return (
-      <div
-        className="box"
-        style={{ color: theme.text, backgroundColor: theme.bg }}
-      >
-        The Box
-      </div>
+      <ThemeContext.Consumer>
+        {context => {
+          const { isLightTheme, light, dark } = context
+          const theme = isLightTheme ? light : dark
+          return (
+            <div
+              className="box"
+              style={{ color: theme.text, backgroundColor: theme.bg }}
+            >
+              The Box
+            </div>
+          )
+        }}
+      </ThemeContext.Consumer>
     )
   }
 }
